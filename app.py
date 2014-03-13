bfad132c485d1e70c677a4fb2fb27ffb87e2bf9c
Vivek Agarwal <me@vivek.im>
2014-03-13 20:09:19 +0530

    Add structure to base template and some re-organization.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, render_template
 from werkzeug import serving
+from flask import Flask, url_for, render_template
+import config
 
 
 # Hiding the file request logs
@@ -10,6 +11,7 @@ serving.WSGIRequestHandler.log_request = log_request
 
 
 app = Flask(__name__, static_url_path='/assets')
+app.config.from_object(config)
 
 app.jinja_env.globals['static'] = lambda filename: url_for('static', filename=filename)
 app.jinja_env.trim_blocks = True
@@ -23,4 +25,4 @@ def serve_file(filename):
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=7779, debug=True)
+    app.run('0.0.0.0', port=7779)

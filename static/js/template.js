ca2f922467dfc14d5a8951884eb99fed20710282
Vivek Agarwal <me@vivek.im>
2014-03-15 02:17:04 +0530

    Add slim sidebar and toggle button.

--- static/js/template.js
+++ static/js/template.js
@@ -20,4 +20,18 @@ $(document).ready(function() {
             parent.children("ul").slideDown(150);
         }
     });
+
+    $(".sidebar-nav-toggle").on('click', function() {
+        var el = $(".sidebar");
+        if (el.hasClass("sidebar-slim-core") || el.hasClass("sidebar-slim")) {
+            el.removeClass("sidebar-slim", 150, function() {
+                el.removeClass("sidebar-slim-core", 150);
+            });
+        } else {
+            el.addClass("sidebar-slim-core", 150, function() {
+                el.addClass("sidebar-slim", 150);
+            });
+        }
+
+    });
 });

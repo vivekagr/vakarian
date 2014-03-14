f121043c9c0e0bf1eb339299ad1314dc799bd752
Vivek Agarwal <me@vivek.im>
2014-03-15 00:08:39 +0530

    Update sidebar children collapsing to use jQuery animations.

--- static/js/template.js
+++ static/js/template.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     var navSidebarLi = $(".nav-sidebar > li");
+    // Adding 'dropdown' class to list items with children
     navSidebarLi.each(function() {
         var el = $(this);
         if (el.children("ul").length > 0) {
             el.addClass("dropdown");
         }
     });
+    // Showing and collapsing the children
     $(".nav-sidebar > li.dropdown > a").on('click', function() {
-        console.log('hey');
-        var el = $(this).parent("li");
-        if (el.hasClass("active")) {
-            el.removeClass("active");
+        var parent = $(this).parent("li");
+        $(".nav-sidebar > li.dropdown").children("ul").slideUp(200);
+        if (parent.hasClass("active")) {
+            parent.removeClass("active");
         } else {
             navSidebarLi.removeClass("active", 1000);
-            el.addClass("active");
+            parent.addClass("active");
+            parent.children("ul").slideDown(200);
         }
     });
 });

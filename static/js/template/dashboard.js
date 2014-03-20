4976d8ad80d24e98301f2e6d6f032e520d7a7c6c
Vivek Agarwal <me@vivek.im>
2014-03-20 21:58:04 +0530

    Add full calendar to the dashboard.

--- static/js/template/dashboard.js
+++ static/js/template/dashboard.js
@@ -2,7 +2,7 @@
 
 $(function() {
 
-    var pageviews, visits, uniques, bounce, df, data, options;
+    var pageviews, visits, uniques, bounce, df, data, options, date, d, m, y, events;
 
     function randrange (min, max) { return Math.random() * (max - min) + min; }
 
@@ -177,6 +177,75 @@ $(function() {
             'OK': { value: 245, tooltip: {content: "Oklahoma - 245 subscribers"} },
             'WA': { value: 1453, tooltip: {content: "Washington - 1453 subscribers"} }
         }
+    });
+
+    // Calendar
+
+    // Setting up the events data for the calendar
+    date = new Date();
+    d = date.getDate();
+    m = date.getMonth();
+    y = date.getFullYear();
+
+    events = [
+        {
+            title: 'All Day Event',
+            start: new Date(y, m, 1)
+        },
+        {
+            title: 'Long Event',
+            start: new Date(y, m, d-5),
+            end: new Date(y, m, d-2)
+        },
+        {
+            id: 999,
+            title: 'Repeating Event',
+            start: new Date(y, m, d-3, 16, 0),
+            allDay: false
+        },
+        {
+            id: 999,
+            title: 'Repeating Event',
+            start: new Date(y, m, d+4, 16, 0),
+            allDay: false
+        },
+        {
+            title: 'Meeting',
+            start: new Date(y, m, d, 10, 30),
+            allDay: false
+        },
+        {
+            title: 'Lunch',
+            start: new Date(y, m, d, 12, 0),
+            end: new Date(y, m, d, 14, 0),
+            allDay: false
+        },
+        {
+            title: 'Birthday Party',
+            start: new Date(y, m, d+1, 19, 0),
+            end: new Date(y, m, d+1, 22, 30),
+            allDay: false
+        },
+        {
+            title: 'Click for Google',
+            start: new Date(y, m, 28),
+            end: new Date(y, m, 29),
+            url: 'http://google.com/'
+        }
+    ];
+
+    // Rendering the calendar
+    $('#calendar').fullCalendar({
+        height: 280,
+        selectable: true,
+        selectHelper: true,
+        editable: true,
+        header: {
+            left: 'title',
+            center: '',
+            right: 'month,agendaWeek,agendaDay prev,next'
+        },
+        events: events
     });
 
 });

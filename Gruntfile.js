d50765f014485c71178ac2786b6e57fa04ffffc8
Vivek Agarwal <me@vivek.im>
2014-04-21 06:53:05 +0530

    Add grunt sass watcher and live reload.

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    lineNumbers: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'static/css/scss',
+                    src: ['*.scss'],
+                    dest: 'static/css/compiled',
+                    ext: '.css'
+                }]
+            }
+        },
+        prettify: {
+            options: {
+                indent: 4,
+                indent_inner_html: false,
+                preserve_newlines: true
+            },
+            all: {
+                expand: true,
+                cwd: 'html/ugly',
+                ext: '.html',
+                src: ['*.html'],
+                dest: 'html/pretty'
+            }
+        },
+        watch: {
+            sass: {
+                files: ['static/css/scss/*.scss'],
+                tasks: ['sass']
+            },
+//            prettify: {
+//                files: ['html/ugly/*.html'],
+//                tasks: ['prettify']
+//            },
+            livereload: {
+                files: ['static/css/compiled/*.css'],
+                options: { livereload: true }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-prettify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 DEBUG = True
+
+HTML_OUTPUT_DIR = 'build'
